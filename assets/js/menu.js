fced29f367d5c54a5b1b0e7ef06603d639ad21bb
AaronHughes424 <aaronhughes424@gmail.com>
2021-11-11 13:32:51 +0000

    changed some css styling and add some cards to start some javascript functions

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,26 +1,67 @@
-// when the page loads the user will be presented with a game menu with buttons
-// when the user clicks on the play button the game menu will be hidden and the
-// game menu will be shown
+window.addEventListener('load', function () {
 
-const gameMenu = document.getElementById('game-menu');
-const playButton = document.getElementById('play-button');
+    // when the page loads the user will be presented with a game menu with buttons
+    // when the user clicks on the play button the game menu will be hidden and the
+    // game menu will be shown
 
-const rulesWindow = document.getElementById('rules-window');
-const rulesButton = document.getElementById('rules-button');
+    const gameMenu = document.getElementById('game-menu');
+    const playButton = document.getElementById('play-button');
 
-const menuButton = document.getElementById('menu-button');
+    const rulesWindow = document.getElementById('rules-window');
+    const rulesButton = document.getElementById('rules-button');
 
-playButton.addEventListener('click', () => {
-    gameMenu.style.display = 'none';
-})
+    const menuButton = document.getElementById('menu-button');
 
-rulesButton.addEventListener('click', () => {
-    gameMenu.style.display = 'none';
-    rulesWindow.style.display = 'flex';
-})
+    const cardButtons = document.querySelectorAll('div.card');
+
+    let clickEnabled = true;
+
+    let firstFlippedCard = null;
+    let secondFlippedCard = null;
+
+    playButton.addEventListener('click', () => {
+        gameMenu.style.display = 'none';
+    })
+
+    rulesButton.addEventListener('click', () => {
+        gameMenu.style.display = 'none';
+        rulesWindow.style.display = 'flex';
+    })
 
 
-menuButton.addEventListener('click', () => {
-    rulesWindow.style.removeProperty('display');
-    gameMenu.style.removeProperty('display');
-})
+    menuButton.addEventListener('click', () => {
+        rulesWindow.style.removeProperty('display');
+        gameMenu.style.removeProperty('display');
+    })
+
+    // game function area
+
+    cardButtons.forEach(function (card) {
+        card.addEventListener('click', function () {
+            flipCard(this);
+        });
+    });
+
+
+
+    function flipCard(card) {
+        if (clickEnabled) {
+            clickEnabled = false;
+            if (firstFlippedCard == null) {
+                firstFlippedCard = card;
+            } else {
+                if (secondFlippedCard == null) {
+                    secondFlippedCard = card;
+                    console.log('Call Compare cards function');
+                    firstFlippedCard = null;
+                    secondFlippedCard = null;
+                }
+            }
+            setTimeout(function () {
+                card.classList.remove('card-back');
+                card.classList.add('card-front');
+                clickEnabled = true;
+            }, 500);
+        }
+    }
+});
